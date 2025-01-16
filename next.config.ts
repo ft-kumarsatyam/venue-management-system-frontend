432abb9d4bf9b626aebd3c9fbfd41916fdd6351e
Kumar Satyam <kumar.satyam@assrm.edu.in>
2025-01-16 11:00:00 +0530

    chore: add configuration files

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "cdn.theorg.com",
+      "encrypted-tbn0.gstatic.com",
+      "staging-api-venue.khelouk.in",
+      "localhost",
+      "www.pngmart.com", 
+      "staging-api-venue.khelouk.in",
+      "cdn.pixabay.com",
+      "media.coliseum-online.com",
+      "i.pinimg.com", 
+      "example.com",
+    ],
+  },
+};
+
+export default nextConfig;
